85d9142fe563e79ad6e123397000a304a73dbb97
albertmas <almaco_io@hotmail.es>
2019-03-30 18:35:56 +0100

    midterm

--- Exercises/midterm.py
+++ Exercises/midterm.py
@@ -5,6 +5,24 @@
 import cv2
 import numpy as np
 
+
+def GaussianFilter(img, kernel):
+    rows, cols, comp = img.shape
+    paddedimg = np.zeros((rows+10, cols+10, comp))
+    paddedimg[5:-5, 5:-5, :] = img.copy()
+    result = img.copy()
+    for x in range(0, rows):
+        for y in range(0, cols):
+            result[x, y] = convolveGaussian(paddedimg, x, y, kernel)
+    return result
+
+
+def convolveGaussian(img, i, j, kernel):
+    k_rows, k_cols = kernel.shape
+    temporal = img[i:i + k_rows, j:j + k_cols]
+    return (temporal * kernel[:, :, np.newaxis]).sum(axis=(0, 1))
+
+
 def ErosionFilter(img):
     rows, cols = img.shape
     paddedimg = np.ones((rows + 2, cols + 2))
@@ -15,7 +33,6 @@ def ErosionFilter(img):
         for y in range(0, cols):
             if img[x, y] != 0:
                 newimg[x, y] = ErodePixel(paddedimg, x+1, y+1)
-
     return np.uint8(newimg)
 
 
@@ -29,39 +46,21 @@ def ErodePixel(img, i, j):
     return 255
 
 
-def convolve(dest, src, i, j, kernel):
-    k_rows, k_cols = kernel.shape
-    srctmp = src[i:i + k_rows, j:j + k_cols]
-    dest[i, j] = (srctmp * kernel[:, :, np.newaxis]).sum(axis=(0, 1))
-
-
-def applyGaussian(img, kernel):
-    rows, cols, comp = img.shape
-    result = img.copy()
-    paddedimage = np.zeros((rows + 10, cols + 10, comp))
-    padding = 5
-    paddedimage[padding:-padding, padding:-padding, :] = img.copy()
-    for i in range(0, rows):
-        for j in range(0, cols):
-            convolve(result, paddedimage, i, j, kernel)
-    return result
-
-
 def exercise1():
     img = np.float64(cv2.imread("noise.jpg", cv2.IMREAD_COLOR))
     rows, cols, comp = img.shape
 
     # TODO: Write your code here
     h_kernel = np.array([1., 10., 45., 120., 210., 252., 210., 120., 45., 10., 1.])
-
     full_h_kernel = np.zeros((11, 11))
     full_h_kernel[5, :] = h_kernel
-    full_h_kernel /= sum(sum(full_h_kernel))
+    full_h_kernel /= full_h_kernel.sum(axis=(0, 1))
 
-    blurred1 = applyGaussian(img, full_h_kernel)
-    cv2.imshow("Blurred1", np.uint8(blurred1))
+    blurred1 = GaussianFilter(img, full_h_kernel)
     full_v_kernel = full_h_kernel.transpose()
-    blurred2 = applyGaussian(blurred1, full_v_kernel)
+    blurred2 = GaussianFilter(blurred1, full_v_kernel)
+
+    cv2.imshow("Blurred1", np.uint8(blurred1))
     cv2.imshow("Blurred2", np.uint8(blurred2))
     cv2.imshow("Original", np.uint8(img))
     cv2.waitKey(0)
@@ -74,7 +73,7 @@ def exercise2():
     # TODO: Write your code here
     filteredimg = ErosionFilter(ErosionFilter(img))
 
-    cv2.imshow("Filtered", filteredimg)
+    cv2.imshow("Output", filteredimg)
     cv2.imshow("Input", img)
     cv2.waitKey(0)
 
